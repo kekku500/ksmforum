4a72e66bee87f770d2a0a59e769be93fdf44e2dc
Kevin Nemeržitski <kevin.nemerzitski@gmail.com>
2015-02-16 21:16:09 +0200

    no message

--- index.php
+++ index.php
@@ -1,2 +1,17 @@
-KSM FOORUM INDEX
-EDIT
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <?php
+        echo 'Hello World (PHP)!';
+        ?>
+    </body>
+</html>
